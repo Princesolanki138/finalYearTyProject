d942ee42b51dc7811c5c7e0a3f7699b4070f8deb
prince solanki <91116758+Princesolanki138@users.noreply.github.com>
2024-03-10 17:21:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js"
+import authRoute from "./routes/authRoute.js";
 
 //configure environment variables
 dotenv.config();
@@ -16,10 +17,17 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+app.use("/api/v1/auth", authRoute);
 
 app.get("/", (req, res) => {
   res.send({
     message: "NS WATCHES",
+  });
+});
+
+app.get("/test1", (req, res) => {
+  res.send({
+    message: "test 1 successfully",
   });
 });
 
