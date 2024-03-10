7c5bea8131ba40350175e21ba60ca321d48f9ac3
prince solanki <91116758+Princesolanki138@users.noreply.github.com>
2024-03-10 18:19:33 +0530

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,15 +1,12 @@
 import express from "express";
+import { registerController, testController } from "../controller/authController.js";
 
 const router = express.Router();
 
 // register post
 router.post("/register", registerController);
 
-//login post
-router.post("/login", loginController)
-
-
-
+router.get("/tester", testController)
 
 
 export default router;
