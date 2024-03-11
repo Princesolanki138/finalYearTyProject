9354e8be85822c73fea264e59ebbbe0f530cb5ec
prince solanki <91116758+Princesolanki138@users.noreply.github.com>
2024-03-11 16:29:19 +0530

    Update authController.js
    made login controller

--- controller/authController.js
+++ controller/authController.js
@@ -1,9 +1,11 @@
-import { hashPassword } from "../helpers/authHelper.js"
+import JWT from "jsonwebtoken"
+import { comparePassd, hashPassword } from "../helpers/authHelper.js"
 import userModel from "../models/userModel.js"
 
+
+//register controller
 export const registerController = async (req, res) => {
   try {
-    console.log(req.body)
     const { name, email, password, phone } = req.body
     //validation 
     if (!name) {
@@ -41,11 +43,67 @@ export const registerController = async (req, res) => {
     console.log(error)
     res.status(500).send({
       success: false,
-      message: "error in Registration",
+      message: "error in register controller",
       error
     })
   }
 }
+
+//login controller
+
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //validation
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid email or password",
+      })
+    };
+    //check user
+    const user = await userModel.findOne({ email })
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Email is not registerd"
+      })
+    }
+    const matchPassd = await comparePassd(password, user.password)
+    if (!matchPassd) {
+      return res.status(200).send({
+        success: false,
+        message: "Wrong Password",
+      })
+    }
+    //token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).send({
+      success: true,
+      message: "login successfully",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+      },
+      token
+    })
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({
+      success: false,
+      message: "error in login controller",
+      error
+    })
+  }
+}
+
+
 
 export const testController = async (req, res) => {
   console.log("protecter")
