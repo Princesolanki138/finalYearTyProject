ddc3a78af5c68be266feafb9a23e039ec4672602
prince solanki <91116758+Princesolanki138@users.noreply.github.com>
2024-03-12 00:54:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,19 @@ app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/product", productRoute);
 
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.sendStatus(200);
+});
 app.get("/", (req, res) => {
   res.send({
     message: "NS WATCHES",
