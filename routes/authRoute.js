a21f2c796820624c370212539001d5825f72b651
prince solanki <91116758+Princesolanki138@users.noreply.github.com>
2024-03-11 16:30:22 +0530

    Update authRoute.js
    set the login routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { registerController, testController } from "../controller/authController.js";
+import { loginController, registerController, testController } from "../controller/authController.js";
 
 const router = express.Router();
 
 // register post
 router.post("/register", registerController);
 
+//login post
+router.post("/login", loginController);
+
+// test
 router.get("/tester", testController)
 
 
