b9a416f543d93b1e995ace2aec23bb1acfa0bf58
prince solanki <solankiprince138@gmail.com>
2024-04-08 00:05:53 +0530

    minor changes in multer.middleware

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -1,7 +1,14 @@
 import multer from "multer";
+import fs from "fs"
+
+const uploadDir = './upload'
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir)
+}
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './upload');
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
